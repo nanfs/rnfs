12851b3f096adf038be51f32c57b3a17611c9049
李帅 <lishuai@cetccloud.com>
2020-06-15 13:36:26 +0800

    [添加] 系统注入配置

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import Router from './router'
 import { Provider } from 'mobx-react'
 import Store from './Store'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 
 ReactDOM.render(
   <Provider store={new Store()}>
-    <Router />
+    <ConfigProvider locale={zhCN}>
+      <Router />
+    </ConfigProvider>
   </Provider>,
   document.getElementById('app')
 )
